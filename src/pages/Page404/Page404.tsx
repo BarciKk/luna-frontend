6fccae888a0b1b226549a3bfe7e1a25a21db41dc
BarciKk <kamil.bartuzel2@o2.pl>
2023-12-02 17:27:31 +0100

    changed styles for the 404 page and created module for this

--- src/pages/Page404/Page404.tsx
+++ src/pages/Page404/Page404.tsx
@@ -1,10 +1,52 @@
-import { Box, Text, Title } from "@mantine/core";
+import {
+  Image,
+  Box,
+  Title,
+  SimpleGrid,
+  Stack,
+  Button,
+  Text,
+} from "@mantine/core";
+import classes from "./Page404.module.css";
+import { Link } from "react-router-dom";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 export const Page404 = () => (
-  <>
-    <Box my="auto" mt="10em" ta="center">
-      <Title order={1}> OPS!</Title>
-      <Text>Sorry, an unexpected error occurred ;x</Text>
-    </Box>
-  </>
+  <Stack h="100dvh" align="center" pt="8em">
+    <SimpleGrid
+      cols={{ base: 1, md: 2 }}
+      spacing="none"
+      className={classes.SimpleGrid}
+    >
+      <Image
+        src="./assets/images/error_page_image.png"
+        maw="550px"
+        visibleFrom="md"
+      />
+      <Box mt="7em">
+        <Title fw="300" order={4} className={classes.title}>
+          ERROR 404
+        </Title>
+        <Title mt="sm" mb="sm" order={1} fz="48" fw="500">
+          Oops!
+        </Title>
+        <Text miw="30%" w="60%" fz={32} fw="bolder">
+          WE COULDN'T FIND THE PAGE YOU'RE LOOKING FOR.
+        </Text>
+        <Link to="/">
+          <Button
+            mt="lg"
+            w="100%"
+            ml="auto"
+            variant="transparent"
+            size="lg"
+            c="headingColors.1"
+            rightSection={<FaArrowRightLong style={{ marginTop: "2px" }} />}
+          >
+            Go home
+          </Button>
+        </Link>
+      </Box>
+    </SimpleGrid>
+  </Stack>
 );
