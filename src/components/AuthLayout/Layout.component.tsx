195645a327c445ff7b54a2cb0592707fb9c234c6
BarciKk <kamil.bartuzel2@o2.pl>
2023-12-02 15:21:29 +0100

    typed layout component prop and created file where i store it

--- src/components/AuthLayout/Layout.component.tsx
+++ src/components/AuthLayout/Layout.component.tsx
@@ -1,8 +1,9 @@
+import { FC } from "react";
 import classes from "./Layout.module.css";
 import { Box, Paper, Stack } from "@mantine/core";
+import { AuthLayoutProps } from "./layoutComponent.types";
 
-export const AuthLayout = ({ children }: any) => {
-  //!type this one
+export const AuthLayout: FC<AuthLayoutProps> = ({ children }) => {
   return (
     <Box className={classes.box}>
       <Stack h="100%" className={classes.stack} justify="center" align="center">
---
+++ src/components/AuthLayout/layoutComponent.types.ts
@@ -0,0 +1,5 @@
+import { ReactNode } from "react";
+
+type AuthLayoutProps = { children: ReactNode };
+
+export type { AuthLayoutProps };
