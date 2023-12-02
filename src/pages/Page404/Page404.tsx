4354d35f30be02250638d5eb1f49bc323941d84e
BarciKk <kamil.bartuzel2@o2.pl>
2023-12-02 17:58:53 +0100

    little fixes for mobile on the page 404

--- src/pages/Page404/Page404.tsx
+++ src/pages/Page404/Page404.tsx
@@ -12,7 +12,7 @@ import { Link } from "react-router-dom";
 import { FaArrowRightLong } from "react-icons/fa6";
 
 export const Page404 = () => (
-  <Stack h="100dvh" align="center" pt="8em">
+  <Stack h="100dvh" align="center" pt="8em" className={classes.Stack}>
     <SimpleGrid
       cols={{ base: 1, md: 2 }}
       spacing="none"
@@ -30,22 +30,25 @@ export const Page404 = () => (
         <Title mt="sm" mb="sm" order={1} fz="48" fw="500">
           Oops!
         </Title>
-        <Text miw="30%" w="60%" fz={32} fw="bolder">
-          WE COULDN'T FIND THE PAGE YOU'RE LOOKING FOR.
-        </Text>
-        <Link to="/">
-          <Button
-            mt="lg"
-            w="100%"
-            ml="auto"
-            variant="transparent"
-            size="lg"
-            c="headingColors.1"
-            rightSection={<FaArrowRightLong style={{ marginTop: "2px" }} />}
-          >
-            Go home
-          </Button>
-        </Link>
+        <Box w="70%">
+          <Text miw={180} fz={32} fw="bolder">
+            WE COULDN'T FIND THE PAGE YOU'RE LOOKING FOR.
+          </Text>
+          <Link to="/">
+            <Button
+              mt="sm"
+              ml="50%"
+              w="fit-content"
+              variant="transparent"
+              p="none"
+              size="lg"
+              c="headingColors.1"
+              rightSection={<FaArrowRightLong style={{ marginTop: "2px" }} />}
+            >
+              Go home
+            </Button>
+          </Link>
+        </Box>
       </Box>
     </SimpleGrid>
   </Stack>
