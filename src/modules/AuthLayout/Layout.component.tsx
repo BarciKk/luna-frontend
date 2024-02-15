f9844a3f0a2c153396f95bdee141682f5ec28d97
BarciKk <kamil.bartuzel2@o2.pl>
2024-02-15 16:55:41 +0100

    installed eslint for further development

--- src/modules/AuthLayout/Layout.component.tsx
+++ src/modules/AuthLayout/Layout.component.tsx
@@ -1,21 +1,21 @@
 import { FC } from "react";
-import { Box, Paper, Stack } from '@mantine/core';
-import classes from './Layout.module.css';
+import { Box, Paper, Stack } from "@mantine/core";
+import classes from "./Layout.module.css";
 import { AuthLayoutProps } from "./layoutComponent.types";
 
 export const AuthLayout: FC<AuthLayoutProps> = ({ children }) => (
-    <Box className={classes.box}>
-      <Stack h="100%" className={classes.stack} justify="center" align="center">
-        <Paper
-          w="100%"
-          mih={600}
-          mah={650}
-          shadow="xl"
-          withBorder
-          className={classes.paper}
-        >
-          {children}
-        </Paper>
-      </Stack>
-    </Box>
+  <Box className={classes.box}>
+    <Stack h="100%" className={classes.stack} justify="center" align="center">
+      <Paper
+        w="100%"
+        mih={600}
+        mah={650}
+        shadow="xl"
+        withBorder
+        className={classes.paper}
+      >
+        {children}
+      </Paper>
+    </Stack>
+  </Box>
 );
